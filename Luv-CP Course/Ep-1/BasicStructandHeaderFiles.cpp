8980e2ffdfd785914525dc2f56af33ac9c16e8eb
atheeq2003 <atheequrrahaman8@gmail.com>
2021-03-09 16:40:57 +0530

    Basics1

---
+++ Luv-CP Course/Ep-1/BasicStructandHeaderFiles.cpp
@@ -0,0 +1,7 @@
+#include<bits/stdc++.h>       // This header file provides all the library files needed for comp prog
+using namespace std;
+int main(){
+    cout << "Hello" << endl;
+    cout << sqrt(16) << endl; // As you can see to print sqrt it requires the use of math.h but here it
+                              // is not required.
+}
