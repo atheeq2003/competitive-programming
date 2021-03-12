0aa4a57ba953ba756b2d30efa78702d3d4248b53
atheeq2003 <atheequrrahaman8@gmail.com>
2021-03-12 16:45:02 +0530

    CharAndASCII

---
+++ Luv-CP Course/Ep-1/CharactersAndASCII.cpp
@@ -0,0 +1,12 @@
+// Characters and ASCII
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    char a = 'c';            
+    char b = 'd';
+    cout << a << endl;
+    cout << b << endl;
+    cout << (int) a << endl; // Use int to print ascii value of an charater
+    cout << (int) b << endl;
+    cout << a - b;           // This prints the difference values of two characters which are ascii values 
+}
