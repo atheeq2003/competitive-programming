b7f3d573484ae617ab3a3394496d5181e38d435c
atheeq2003 <atheequrrahaman8@gmail.com>
2021-03-13 13:09:22 +0530

    PracQues

---
+++ Luv-CP Course/Ep-1/PracticeQuestions/3_PracQues3.cpp
@@ -0,0 +1,14 @@
+// Add three numbers
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    long int a;
+    long int b;
+    long int c;
+    long int sum;
+    cin >> a;
+    cin >> b;
+    cin >> c;
+    sum = a + b + c;
+    cout << sum << endl;
+}
