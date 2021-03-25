db9287083332dbb35cfff428bdb4bad7453c5491
atheeq2003 <atheequrrahaman8@gmail.com>
2021-03-25 19:35:41 +0530

    ForLoop

--- Luv-CP Course/Ep-3/4_ForLoop.cpp
+++ Luv-CP Course/Ep-3/4_ForLoop.cpp
@@ -2,9 +2,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int n;
-    cin >> n;
-    for(int i = 1; i <= n; i++){
+    int x;
+    cin >> x;
+    for(int i = 1; i <= x; i++){
         for(int j = 1; j <= i; j++){
             cout << "*";
         }
