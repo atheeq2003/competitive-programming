1c982874ee2fcef4edc4de01a79ec85dddfc4bf5
atheeq2003 <atheequrrahaman8@gmail.com>
2021-03-26 18:12:33 +0530

    LifeUniverseAndEverything

---
+++ Luv-CP Course/Ep-3/5_LifeUniverseAndEverything.cpp
@@ -0,0 +1,14 @@
+// Life Universe And Everything
+#include<bits/stdc++.h>
+using namespace std;
+int main(){ 
+    while(true){
+        int n;
+        cin >> n;
+        if(n == 65){
+            break;
+        }
+        cout << n << endl;
+    }
+    
+} 
