43a2410d4c32c8b4859b388b76f9e353d56d1ab4
atheeq2003 <atheequrrahaman8@gmail.com>
2021-04-03 23:06:24 +0530

    SizeLimitsInArray

--- Luv-CP Course/Ep-5/3_SizeLimitsInArray.cpp
+++ Luv-CP Course/Ep-5/3_SizeLimitsInArray.cpp
@@ -1,8 +1,8 @@
 #include<bits/stdc++.h>
 using namespace std;
-const int n = 2e7;             // Global Declaration does not lead to segmentation fault
+const int n = 1e7;             // Global Declaration does not lead to segmentation fault
 int a[n];                      // And also value cannot be changed because of const 
 int main(){
-    a[n-1] = 7;                // Declaring in main give seg fault
-    cout << a[n-1];
+    a[0] = 2;                  // Declaring in main give seg fault
+    cout << a[0];
 }
