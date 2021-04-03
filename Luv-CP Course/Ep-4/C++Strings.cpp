38b8d25073dcc391a20231348a7f104b796ec7fd
atheeq2003 <atheequrrahaman8@gmail.com>
2021-04-03 18:21:19 +0530

    C++Strings

--- Luv-CP Course/Ep-4/C++Strings.cpp
+++ Luv-CP Course/Ep-4/C++Strings.cpp
@@ -1,9 +1,9 @@
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    string str1 = "Hello";
-    cout << str1 << endl;
-    string str2;
-    cin >> str2;
-    cout << str2;
+    string str = "Hello";
+    cout << str << endl;
+    string str1;
+    cin >> str1;
+    cout << str1;
 }
