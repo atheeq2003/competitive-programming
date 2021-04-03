feae32d7e69947efee241625f579675fadaf427f
atheeq2003 <atheequrrahaman8@gmail.com>
2021-04-03 20:40:48 +0530

    PalindromeInString

---
+++ Luv-CP Course/Ep-4/6_PalindromeInStrings.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    string str;
+    cin >> str;
+    string strrev;
+    for(int i = str.size() - 1; i >= 0; i--){
+        strrev.push_back(str[i]);
+    } 
+    if(str == strrev){
+        cout << "Palindrome" << endl;
+    }else{
+        cout << "Not A Palindrome" << endl;
+    }
+}
