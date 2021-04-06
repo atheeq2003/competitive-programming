4799b2814b5b3ebd74d3239bb233bdbe2865059b
atheeq2003 <atheequrrahaman8@gmail.com>
2021-04-06 17:33:33 +0530

    Pointers

---
+++ Luv-CP Course/Ep-7/Pointers.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int x = 4;
+    int *p_x = &x;
+    cout << "Adr of x: " << &x << endl;
+    cout << "Adr of p_x: " << p_x << endl;
+    cout << "Val od *p_x: " << *p_x << endl;
+    *p_x = 5;
+    cout << x << endl;
+    cout << "p_x + 1" << p_x + 1 << endl;
+}
